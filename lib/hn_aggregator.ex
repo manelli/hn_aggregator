3f77b469c1ffb079a47338bcecf9e0ea14e37550
Martin Manelli <manelli.ml@gmail.com>
2022-12-05 00:03:35 -0300

    Create new hn_aggregator project.
    This application was created using the following command:
    `mix new hn_aggregator --module HNAggregator --sup`

---
+++ lib/hn_aggregator.ex
@@ -0,0 +1,18 @@
+defmodule HNAggregator do
+  @moduledoc """
+  Documentation for `HNAggregator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HNAggregator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hn_aggregator_test.exs
@@ -0,0 +1,8 @@
+defmodule HNAggregatorTest do
+  use ExUnit.Case
+  doctest HNAggregator
+
+  test "greets the world" do
+    assert HNAggregator.hello() == :world
+  end
+end
